1ceae64f14e2162fbbfc99fa11dfae0c480eeaa7
Vladimir_Korolevskii <vladimir_korolevskii@epam.com>
2026-07-07 08:18:04 +0200

    Initial Commit

---
+++ stopwatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stopwatch
+//
+//  Created by Vladimir Korolevskii on 7. 7. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stopwatch/stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopwatchApp.swift
+//  stopwatch
+//
+//  Created by Vladimir Korolevskii on 7. 7. 2026..
+//
+
+import SwiftUI
+
+@main
+struct stopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
